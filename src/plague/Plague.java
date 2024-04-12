02661004f34bba4051f63842e21d7fe597228bf2
-Ace <143662232+minusAce@users.noreply.github.com>
2024-04-12 13:37:12 -0700

    Update Plague.java

--- src/plague/Plague.java
+++ src/plague/Plague.java
@@ -1,8 +1,22 @@
-//package plague;
-//
-//import simstation.Agent;
-//
-//public class Plague extends Agent {
-//    @Override
-//    public void update() {}
-//}
+package plague;
+
+import mvc.Utilities;
+import simstation.Agent;
+
+public class Plague extends Agent {
+    boolean infected;
+    @Override
+    public void update() {
+        int steps = Utilities.rng.nextInt(10) + 1;
+        move(steps);
+        Plague other = (Plague) world.getNeighbor(this, 10);
+        if (other != null && other.infected && !this.infected) {
+            int infected = Utilities.rng.nextInt(2);
+            if (infected == 1)
+            {
+                int resisted = Utilities.rng.nextInt(100);
+                this.infected = resisted != 0 && resisted != 1;
+            }
+        }
+    }
+}
