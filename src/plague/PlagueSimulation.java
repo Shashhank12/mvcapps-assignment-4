e58ca14c53fd48861ab451c096c5f3a4ef7b10b3
-Ace <143662232+minusAce@users.noreply.github.com>
2024-04-12 13:37:19 -0700

    Update PlagueSimulation.java

--- src/plague/PlagueSimulation.java
+++ src/plague/PlagueSimulation.java
@@ -1,5 +1,7 @@
 package plague;
 
+import mvc.Utilities;
+import simstation.Agent;
 import simstation.Simulation;
 
 public class PlagueSimulation extends Simulation {
@@ -7,5 +9,40 @@ public class PlagueSimulation extends Simulation {
     public static int RESISTANCE = 2;
 
     @Override
-    public void populate() {}
+    public void populate() {
+        for (int i = 0; i < 20; i++) {
+            Plague p = new Plague();
+            int infected = Utilities.rng.nextInt(2);
+            if (infected == 1)
+            {
+                int resisted = Utilities.rng.nextInt(100);
+                p.infected = resisted != 0 && resisted != 1;
+            }
+            else {
+                p.infected = false;
+            }
+            addAgent(p);
+        }
+    }
+
+    public int getInfectedAmount()
+    {
+        int infectedAmount = 0;
+        for (Agent a: getAgents())
+        {
+            if (a instanceof Plague p) {
+                if (p.infected) {
+                    infectedAmount++;
+                }
+            }
+        }
+        return infectedAmount;
+    }
+
+    @Override
+    public String[] getStats()
+    {
+        float infectedPercent = (float) getInfectedAmount() / getAgents().size();
+        return new String[] {"Agents: " + getAgents().size(),"Clock: " + clock, "% Infected: " + infectedPercent};
+    }
 }
