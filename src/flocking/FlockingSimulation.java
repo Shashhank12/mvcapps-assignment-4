e12facce3410d4afed98d2006189fec9aaef3537
priyankagoel <gunjan.priyanka29@gmail.com>
2024-04-13 19:53:53 -0700

    [Flocking] Added customization of Simulation: FlockingSimulation

--- src/flocking/FlockingSimulation.java
+++ src/flocking/FlockingSimulation.java
@@ -3,11 +3,20 @@ package flocking;
 import simstation.Simulation;
 
 public class FlockingSimulation extends Simulation {
+    private static final long serialVersionUID = 1898762915297837586L;
+
     @Override
-    public void populate() {}
+    public void populate() {
+        for(int i = 0; i < 100; i++) {
+            addAgent(new Bird("Bird "+i));
+        }
+    }
 
     @Override
     public String[] getStats() {
+        for(int i = 0; i < 100; i++) {
+            //
+        }
         return null;
     }
 }
