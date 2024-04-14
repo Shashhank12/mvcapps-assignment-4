7cbef4cc35b0384efddfc6f80e494b0ad39800c1
priyankagoel <gunjan.priyanka29@gmail.com>
2024-04-13 19:54:58 -0700

    [Flocking] Added customization of SimulationPanel: FlockingPanel

---
+++ src/flocking/FlockingPanel.java
@@ -0,0 +1,14 @@
+package flocking;
+import mvc.AppFactory;
+import simstation.SimulationPanel;
+
+public class FlockingPanel extends SimulationPanel {
+    public FlockingPanel(AppFactory factory) {
+        super(factory);
+    }
+    public static void main(String[] args) {
+        AppFactory factory = new FlockingFactory();
+        FlockingPanel panel = new FlockingPanel(factory);
+        panel.display();
+    }
+}
